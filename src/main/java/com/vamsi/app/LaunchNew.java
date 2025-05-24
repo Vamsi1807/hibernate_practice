4dcafc39f2f2a86543df19d5e7c4e86944f60967
Vamsi1807 <vamsigopavarapu07@gmail.com>
2025-05-24 12:44:53 +0530

    new updations

--- src/main/java/com/vamsi/app/LaunchNew.java
+++ src/main/java/com/vamsi/app/LaunchNew.java
@@ -10,49 +10,51 @@ import com.vamsi.model.Student;
 
 public class LaunchNew {
 
-	public static void main(String[] args) {
-		Configuration config=null;
-		SessionFactory sf=null;
-		Transaction tr=null;
-		Session session=null;
-		boolean flag=false;
-		try {
-			config = new Configuration();
-			sf = config.buildSessionFactory();
-			session = sf.openSession();
-			tr = session.beginTransaction();
-			
-			
-			Student stu = new Student();
-			stu.setName("Siddhu");
-			stu.setId(4343);
-			stu.setCity("Mutukuru");
-			
-			
-			session.persist(stu);
-			
-			flag=true;
-			
-			
-		}
-		catch(HibernateException e){
-			e.printStackTrace();
-		}
-		catch(Exception a){
-			a.printStackTrace();
-		}
-		
-		finally {
-			if(true) {
-				System.out.println("Student object saved successfully...");
-			}
-			else {
-				System.out.println("Error...");
-			}
-			session.close();
-			sf.close();
-		}
+    public static void main(String[] args) {
+        Configuration config = null;
+        SessionFactory sf = null;
+        Transaction tr = null;
+        Session session = null;
+        boolean flag = false;
+        try {
+            config = new Configuration().configure(); // Hibernate will automatically look for hibernate.cfg.xml
+            sf = config.buildSessionFactory();
+            session = sf.openSession();
+            tr = session.beginTransaction();
 
-	}
+            Student stu = new Student();
+            stu.setName("Siddhu");
+            stu.setId(4343);
+            stu.setCity("Mutukuru");
+
+            session.persist(stu);
+
+            tr.commit(); // Commit the transaction if everything is successful
+            flag = true;
+
+        } catch (HibernateException e) {
+            if (tr != null) {
+                tr.rollback(); // Rollback the transaction if an error occurs
+            }
+            e.printStackTrace();
+        } catch (Exception a) {
+            if (tr != null) {
+                tr.rollback();
+            }
+            a.printStackTrace();
+        } finally {
+            if (flag) {
+                System.out.println("Student object saved successfully...");
+            } else {
+                System.out.println("Error occurred while saving the student.");
+            }
+            if (session != null) {
+                session.close();
+            }
+            if (sf != null) {
+                sf.close();
+            }
+        }
+    }
 
 }
